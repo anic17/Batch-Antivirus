34f7e271d933908f2b110786e2bcd3fc1eab9034
anic17 <58483910+anic17@users.noreply.github.com>
2020-12-28 20:18:08 +0100

    Fixed findstr keys bug

--- DeepScan.bat
+++ DeepScan.bat
@@ -1,13 +1,15 @@
+::BAV_:git@github.com:anic17/Batch-Antivirus.git
+
 @echo off
+if "%~f1"=="%~f0" echo Safe file (Whitelisted). && exit /b
 setlocal EnableDelayedExpansion
 set dir=%CD%
 path=%PATH%;%CD%
 set ver=0.1.0
-
+set report=1
 set "string[severe]=Severe malware found."
 set "string[malware]=Malware found."
-set "string[suspicious]=Suspicious file."
-set "string[possibly]=Possibly clean file."
+set "string[possibly]=Suspicious indicator in file."
 set "string[clean]=Clean file."
 set "string[safe]=Safe file."
 
@@ -19,31 +21,52 @@ if not defined argv[1] (
 )
 
 if /i "%~1"=="--help" goto help
-title Batch AntiVirus Heuristic Analyser
+title Batch Antivirus Heuristic Analyser
 
 set admin=1
 set ratio=0
 set threats=0
 
-net session 2>nul 1>nul || set admin=0
+(net session 2>nul 1>nul && set "admin=1")|| set admin=0
 
 echo.
-echo Scanning file...
-echo.
+for /f %%A in ('copy /Z "%~dpf0" nul') do set "CR=%%A"
+<nul set /p "=Scanning file..."
+
 set "current_dir=%CD%"
-echo on
+
 set "filescan=%~f1"
 set head=
 set head2=
-set "keys_findstr=uy9zQrvd %ÂdfsxÀp:$q,h*OanÅÊ|Î"1/n0 0".ÄÁ~ºÈi}2*S~1*""nKa%c"K84"0H *$0fd,1cq""pH¨*Þ*ft~þ<*k+þB"b%::K¨a%"~:¿/Z  uÀÊ%0yci&­0Ã^*|.&c%ßT%/"ÄÛr|%/yC}C~i&Lw@]["b'p0EC kFËþ0tT%3> Qº\GÈn %0dt|©%6Ar~f"°/xnz"//È²*X%/º~l¹4:dE#*pJ¼|$-;Û%fc,O|%Kºl¼Op~gzx0#bÈÝ^"%*ÅEY'd6 fc%;j:0/cY*0/Í~ËÎkiÛr-c*]3:~gcnfex//R"y±1Ã@&~º(7*,"CV¼Zx%0~U0q|0"
+for /f "usebackq eol=; delims=" %%A in ("%filescan%") do if not defined head (set "head=%%A") else if not defined head2 set "head2=%%A" && goto getting_wl
 
-findstr /r /i /c:"%%0*|*%%0" /c:"%%~f0*|*%%~f0" /c:"%%~dpnx0*|*%%~dpnx0" /c:"%%~f0*|*%%0" /c:"%%0*|*%%~f0" /c:"%%~dpnx0*|*%%~f0" /c:"%%~f0*|*%%~dpnx0" "%filescan%" > nul 2>&1 && echo.!string[malware]! && exit /b
+:getting_wl
+set obfuscated=
+set bfp=
+::if "%head:~0,2%"=="MZ" goto scanexe
+::echo.%HEAD%
+
+
+if "%head%"=="::BAV_:git@github.com:anic17/Batch-Antivirus.git" (
+	echo.!cr!Scan finished.   
+	echo.
+	echo Safe file.
+	exit /b 0
+)
+
+
+findstr /i /c:"%%0|%%0" /c:"%%~f0|%%~f0" /c:"%%~dpnx0|%%~dpnx0" /c:"%%~f0|%%0" /c:"%%0|%%~f0" /c:"%%~dpnx0|%%~f0" /c:"%%~f0|%%~dpnx0" "%filescan%" > nul 2>&1 && echo.!string[malware]! && exit /b
 if not exist "%filescan%" (
 	echo.Could not find '%filescan%'
 	exit /b 1
 )
-for /f "usebackq eol=: delims=" %%A in ("%filescan%") do if not defined head (set "head=%%A") else if not defined head2 set "head2=%%A"
 
+
+if /i "%~x1"==".cmd" goto scanbatch
+if /i "%~x1"==".bat" goto scanbatch
+
+:gothead
+if "%head%"==":BFP" set bfp=1
 set "head=!head:"=!"
 set "head=!head:&=!"
 set "head=!head:@=!"
@@ -55,27 +78,23 @@ set "head_tok1=!head_tok1:&=!"
 set "head_tok2=!head_tok2:&=!"
 set "head_tok1=!head_tok1:@=!"
 set "head_tok2=!head_tok2:@=!"
-if /i "%head%"=="echo off" (set "mime=application/x-bat") else if /i "%head_tok1%"=="echo" (set "mime=application/x-bat") else if "%head_tok1%"=="shift" (set "mime=application/x-bat") else set "mime=bin"
-if "%mime%"=="application/x-bat" goto scanbatch
-exit /b
 
-exit /b
-echo yep lol
-findstr /r /i /c:"*\.*\.*\.*" /c:"http://" /c:"www\." /c:"https://" "%filescan%"
-findstr /r "%filescan%"
+for %%A in ("echo" "if exist" "shift" "prompt" "title" "setlocal") do if "%%~A"=="!head!" set "mime=application/x-bat"
+if "!mime!"=="application/x-bat" goto scanbatch
 
+for %%A in ("#^!/usr/bin/python" "#^!/bin/python" "from" "import"
 
+) if "%%~A"=="!head_tok1!" set "mime=text/python
 
+set mime=bin
 
-
-
-
-echo yep lol 2
+echo.MIME is !mime!
+rem echo this is an exe file
 exit /b
 
 
-:: Get file hash
-for /f %%A in ('sha256.exe "%~1" 2^>nul') do (set "hash=%%A" & goto scan)
+
+exit
 
 :hashed
 
@@ -100,15 +119,8 @@ icacls %filescan% /grant %username%:(F,MA,WA,RA,WEA,REA,WDAC,DE) 2>nul 1>nul
 move %filescan% "%DIR%\Data\Quarantine\!hash!\!hash!" /y 2>nul 1>nul
 icacls "%DIR%\Data\Quarantine\!hash!\!hash!" /deny %username%:(RX,W,R,M,RD,WEA,REA,X,RA,WA) 2>nul 1>nul
 set /a threats+=1
-if not exist %filescan% (echo Malware successfully quarantined) else (echo Error while deleting malware)
+if not exist %filescan% (echo Malware successfully quarantined) else (echo Error while quarantining malware)
 goto :EOF
-:not_found_possible_malware
-
-
-
-
-
-
 
 echo Scanned "%~dpnx1" and found !threats! threats
 pause>nul
@@ -151,34 +163,135 @@ icacls %filescan% /grant %username%:(F,MA,WA,RA,WEA,REA,WDAC,DE) 2>nul 1>nul
 if /i "%delmalware%"=="y" del !filescan! /s /q > nul
 echo.
 goto :EOF
-
 :scanbatch
-set "mimikatz_key=±-Âb-,znI<tiWPY4wNb)Ùpr»y=^MeTP0-i}oAk-Êkvma*°;"
-set "text="
-for %%A in (8;7;41=35;40;28,38=27,11 42 33 37 43=10 6) do set "text=!text!!mimikatz_key:~%%A,1!"
-set ratio=0
-:: 1 + 3 + 1 + 4 + 1 + 1 + 4 + 5 = 20
-findstr /r /i /c:"*.*.*.*" /c:"http://" /c:"www\." /c:"https://" /c:"ftp://" /c:"sftp://" "%filescan%" > nul 2>&1 && set /a ratio+=1
-findstr /i /c:"del %HomeDrive%\*" /c:"del %%HomeDrive%%\*"/c:"erase %HomeDrive%\*" /c:"erase %%HomeDrive%%\*" "%filescan%" > nul 2>&1 && set /a ratio+=3
-findstr /ic:"ping www.*.*" "%filescan%" > nul 2>&1 && set /a ratio+=1
-findstr /ic:"!text!" "%filescan%" > nul 2>&1 && set /a ratio+=9
-findstr /ic:"vssadmin" "%filescan%" > nul 2>&1 && set /a ratio+=1
-findstr /ic:"bcdedit" "%filescan%" > nul 2>&1 && set /a ratio+=1
-findstr /r /i /c:"taskkill * /im explorer.exe *" "%filescan%" > nul 2>&1 && set /a ratio+=1
-findstr /r /i /c:"taskkill*/im csrss.exe*" /c:"taskkill * /im System *" /c:"taskkill*/im wininit.exe *" /c:"taskkill * /im svchost.exe *"  /c:"taskkill * /im services.exe *" "%filescan%" > nul 2>&1 && set /a ratio+=5
 
-if %ratio% equ 20 (
+
+
+set "in2=@if defined"
+set "batch_=_out"
+
+
+findstr /ic:"%in2%%batch_% %%%%~G" "%filescan%" > nul 2>&1 && (set "in2batch=1") || (set "in2batch=0")
+
+for /f %%A in ('gethex.exe "%filescan%" 100') do set "head=%%A"
+if /i "%head:~0,2%"=="feff" set "obfuscated=1"&set "obfc=FE FF"
+if /i "%head:~0,2%"=="fffe" set "obfuscated=1"&set "obfc=FF FE"
+if /i "%head:~0,4%"=="fffe0000" set "obfuscated=1"&set "obfc=FF FE 00 00"
+
+if "%head:~0,4%"=="��  " set "obfuscated=1"&set "ofc=FF FE 00 00"&set "extra_info=FF FE 00 00"
+if "%head:~0,2%"=="��" set "obfuscated=1"&set "obfc=FE FF"&set "extra_info=FE FF"
+if "%head:~0,2%"=="��" set "obfuscated=1"&set "obfc=FF FE"&set "extra_info=FF FE"
+if "%head:~0,4%"==":BFP" set bfp=1
+if "%head:~0,2%"=="MZ" (echo You are reading a binary file.&exit/B)
+if "%BFP%"=="1" set head=
+if "%BFP%"=="1" (
+	
+	del "%TMP%\bav_deepscan.bfp" /q > nul 2>&1
+	del "%TMP%\bav_deepscan.b64" /q > nul 2>&1
+	del "%TMP%\bav_deepscan.file" /q > nul 2>&1
+	findstr /vc:"echo." /c:"echo " /c:"for " /c:":" /c:"rem " /c:"certutil -" /c:")" /c:"del " /c:"expand" /c:"%%*" /vc:"erase" "%filescan%" | findstr /vc:"-" /c:"_" /c:"\" /c:" > "%TMP%\bav_deepscan.b64"
+
+	certutil -decode -f "%TMP%\bav_deepscan.b64" "%TMP%\bav_deepscan.bfp"  > nul
+	expand "%TMP%\bav_deepscan.bfp" "%TMP%\bav_deepscan.file" > nul
+	set "filescan=%TMP%\bav_deepscan.file"
+	del "%TMP%\bav_deepscan.bfp" /q > nul 2>&1
+	del "%TMP%\bav_deepscan.b64" /q > nul 2>&1
+)
+if "%in2batch%"=="1" (
+	findstr /bic:"echo " "%filescan%" > "%TMP%\bav.deepscan.in2batch"
+)
+if "%BFP%"=="1" for /f "usebackq delims=" %%A in ("!filescan!") do if not defined head (set "head=%%A") else if not defined head2 set "head2=%%A" && goto getting_wl_bfp
+if defined bfp set "extra_info=BFP"
+:getting_wl_bfp
+for /f %%A in ('gethex.exe "%filescan%" 100') do set "head=%%A"
+if /i "%head:~0,2%"=="feff" set "obfuscated=1"&set "obfc=FE FF"
+if /i "%head:~0,2%"=="fffe" set "obfuscated=1"&set "obfc=FF FE"
+if /i "%head:~0,4%"=="fffe0000" set "obfuscated=1"&set "obfc=FF FE 00 00"
+
+
+
+if "%head%"=="3a3a4241565f3a676974406769746875622e636f6d3a616e696331372f42617463682d416e746976697275732e676974" (
+	echo.!cr!Scan finished.  
+	echo.
+	echo.Safe file.
+	exit /b 0
+)
+set "mk_key=Y�f�mzkkmt-Yiika"
+set "text="
+for %%A in (4=12=8,13;7,15=9=5,) do set "text=!text!!mk_key:~%%A,1!"
+set ratio=0
+
+set "pn=pin"
+set "ic=icac"
+set "sch=schta"
+set "net=nets"
+set "psx=psexe"
+set "bcde=bcded"
+set "vssa=vssadm"
+
+
+findstr /vc:"echo" /vc:":" /ivc:"rem" "%filescan%" | findstr /i /c:"*\.*\.*\.*" /c:"http://" /c:"www\." /c:"https://" /c:"ftp://" /c:"sftp://" /c:"cURL" /c:"wget" /c:"Invoke-WebRequest" /c:"bitsadmin" /c:"certutil -urlcache" /c:"createobject(\"Microsoft\.XMLHTTP\")"> nul 2>&1 && set /a ratio+=1 && set "report_http_ftp=Contacts an FTP server/makes an HTTP request (+1^)"
+findstr /i /c:"del *" /c:"del %%HomeDrive%%\*" /c:"erase %HomeDrive%" /c:"erase %%HomeDrive%%\*" "%filescan%" > nul 2>&1 && set /a ratio+=3 && set "report_delete=Deletes files (+3^)"
+
+findstr /vic:"echo" "%filescan%" | findstr /bic:"%pn%g " /c:"%pn%g.exe ">nul 2>&1 && set /a ratio+=1 && set "report_ping=Pings website/IP (+1^)"
+findstr /vic:"echo" "%filescan%" | findstr /bic:"%ic%ls " /c:"%ic%ls.exe ">nul 2>&1 && set /a ratio+=1 && set "report_icacls=Changes ACL of a file or directory (+1^)"
+findstr /vic:"echo" "%filescan%" | findstr /bic:"%sch%sks " /c:"%sch%sks.exe ">nul 2>&1 && set /a ratio+=1 && set "report_schtasks=Modifies scheduled tasks (+1^)"
+findstr /vic:"echo" "%filescan%" | findstr /bic:"%net%h " /c:"%net%h.exe " >nul 2>&1 && set /a ratio+=1 && set "report_netsh=Runs network shell to edit or get configuration (+1^)"
+findstr /vic:"echo" "%filescan%" | findstr /bic:"psexec " /c:"%psx%c." /c:"%psx%c64 " /c:"%psx%c64." >nul 2>&1 && set /a ratio+=1 && set "report_psexec=Uses PSExec to run remote commands"
+
+:: Find Mimikatz string (encoded to evit self-false positives)
+findstr /ic:"!text:_=-!" "%filescan%" > nul 2>&1 && set /a ratio+=5 && set "report_mimikatz=Uses HackTool/Mimikatz  (+5^)"
+findstr /ic:"%vssa%in " "%filescan%" > nul 2>&1 && set /a ratio+=1 && set "report_vssadmin=Uses VSSAdmin command to manage shadow copies (+1^)"
+findstr /ic:"%bcded%it " "%filescan%" > nul 2>&1 && set /a ratio+=1 && set "report_bcdedit=Uses BCDEdit command to edit boot configuration data (+1^)"
+findstr /bvc:"echo" /vc:":" /ivc:"rem" /bvc:"echo" "%filescan%" 2>nul | findstr /ic:"taskkill /f /im " /c:"taskkill /im" /c:"taskkill /fi" /c:"taskkill /pid" /c:"taskkill /f" /c:"pskill " /c:"pskill.exe" /c:"pskill64 " /c:"tskill " /c:"tskill.exe" "%filescan%" > nul 2>&1 && set /a ratio+=1 && set "report_taskkill=Finishes processes (+1^)" && findstr /i /c:"csrss" /c:"wininit" /c:"svchost" /c:"services" /c:"explorer" /c:"msmpeng" /c:"%filescan%" > nul 2>&1 && set /a ratio+=5 && set "report_taskkill_critical=Finishes system critical processes (+5^)"
+
+echo.!cr!Scan finished.   
+echo.
+if "%report%" equ "1" (
+	echo Batch Antivirus report:
+	echo.
+	if %ratio% equ 0 echo No suspicious indicators found.
+	if defined report_bcdedit echo.%report_bcdedit%
+	if defined report_delete echo.%report_delete%
+	if defined report_http_ftp echo.%report_http_ftp%
+	if defined report_mimikatz echo.%report_mimikatz%
+	if defined report_ping echo.%report_ping%
+	if defined report_icacls echo.%report_icacls%
+	if defined report_schtasks echo.%report_schtasks%
+	if defined report_netsh echo.%report_netsh%
+	if defined report_taskkill echo.%report_taskkill%
+	if defined report_taskkill_critical echo.%report_taskkill_critical%
+	if defined report_vssadmin echo.%report_vssadmin%
+	if defined report_psexec echo.%report_psexec%
+	if defined extra_info (
+		echo.
+		echo Extra information:
+		echo.
+		if defined bfp echo File packed using Batch File Packer ^(BFP^)
+		if defined in2batch echo File packed using In2Batch
+		if defined obfuscated echo Batch file obfuscated using %obfc% hexadecimal characters
+		echo.
+	)
+	echo.
+	echo Ratio: %ratio%/20
+	echo.
+	<nul set /p "=Veridict: "
+)
+:: 12/20+ considered malicious
+::set ratio=12
+::echo on
+for %%A in (bcdedit delete http_ftp mimikatz ping icacls schtasks netsh taskkill taskkill_critical vssadmin psexec) do if defined report_%%A set ai_%%A=1
+
+	
+if %ratio% geq 18 (
 	echo.!string[severe]!
 	exit /b %ratio%
 )
-if %ratio% leq 19 if %ratio% geq 15 (
+if %ratio% leq 17 if %ratio% geq 10 (
 	echo.!string[malware]!
 	exit /b %ratio%
 )
-if %ratio% leq 14 if %ratio% geq 10 (
-	echo.!string[suspicious]!
-	exit /b %ratio%
-)
+
 if %ratio% leq 9 if %ratio% geq 5 (
 	echo.!string[possibly]!
 	exit /b %ratio%
@@ -192,3 +305,23 @@ if %ratio% equ 0 (
 	exit /b %ratio%
 )
 exit /b %ratio%
+:: In case of failure, exit with error code 1
+exit 1
+
+:help
+echo.
+echo Batch Antivirus - DeepScan
+echo.
+echo Syntax:
+echo.
+echo DeepScan ^<filename^>
+echo.
+echo Example:
+echo.
+echo DeepScan script.bat
+echo.
+echo Will return the malware detection code (0 means safe, 20 means severe malware)
+echo and will print report
+echo.
+echo Copyright (c) 2020 anic17 Software
+exit/B
