2c1c58f8540f0240cd78ecf9aabd98d750c7726a
anic17 <58483910+anic17@users.noreply.github.com>
2023-05-04 09:09:36 +0200

    v3.1.0 - Bug fixes in USB Cleaner

--- USBCleaner.bat
+++ USBCleaner.bat
@@ -3,13 +3,12 @@
 setlocal EnableDelayedExpansion
 pushd "%~dp0"
 title Batch Antivirus Shortcut Malware Remover
-echo Batch Antivirus drive shortcut malware remover
+echo Batch Antivirusd drive shortcut malware remover
 echo.
 echo.This utility will help in the removal of the widely-spread USB shortcut malware.
-echo.
 echo.It will also restore the original files.
 echo.
-set /p "Drive=Drive to disinfect > "
+set /p "Drive=Drive to disinfect:  "
 if not exist "%Drive::=%:" (
 	echo Could not find %Drive::=%: drive
 	pause>nul
@@ -17,39 +16,41 @@ if not exist "%Drive::=%:" (
 )
 echo Scan started...
 set cnt=0
-if not exist "!Drive::=!:\*.lnk" (
-	if not exist "!Drive::=!:\.Trashes" (
-		echo.
-		echo Drive is not infected
-		pause>nul
-		exit /b
-	)
-)
-set "threatname=JS/Bondat"
-set cnt_dir=0
-echo Warning^^!: Drive infected (%threatname%)
-echo.
-echo Removing %threatname% ...
-md "%~dp0Data\Quarantine\USB\!Drive::=!"  > nul 2>&1
-attrib -h -s /d /s "!Drive::=!:\*.*"  > nul 2>&1
-move /y "!Drive::=!:\*.lnk" "%~dp0Data\Quarantine\USB\!Drive::=!" > nul 2>&1
-move /y "!Drive::=!:\*.exe" "%~dp0Data\Quarantine\USB\!Drive::=!" > nul 2>&1
-move /y "!Drive::=!:\*.vbs" "%~dp0Data\Quarantine\USB\!Drive::=!" > nul 2>&1
-move /y "!Drive::=!:\.trashes" "!Drive::=!:\" > nul 2>&1
 if exist "!Drive::=!:\*.lnk" (
-	echo Clean failed^^!
-) else (
-	echo !threatname! was successfully removed from your system.
-	echo.
-	echo It is recommended to run a Batch Antivirus scan to there's no malware left in the system
-	echo.Do you want to run a scan on !Drive::=!: ? (y/n)
-	choice /c:YN /n
-	if !errorlevel!==1 (
-		echo Running scan...
-		BAV "%!Drive::=!:\"
-		exit /b %errorlevel%
+	if exist "!Drive::=!:\.Trashes" (
+		set "threatname=JS/Bondat"
+		set cnt_dir=0
+		echo Warning^^!: Drive infected ^(!threatname!^)
+		echo.
+		echo Removing !threatname! ...
+		md "%~dp0Data\Quarantine\USB\!Drive::=!"  > nul 2>&1
+		attrib -h -s /d "!Drive::=!:\*.*"  > nul 2>&1
+		move /y "!Drive::=!:\*.lnk" "%~dp0Data\Quarantine\USB\!Drive::=!" > nul 2>&1
+		move /y "!Drive::=!:\*.exe" "%~dp0Data\Quarantine\USB\!Drive::=!" > nul 2>&1
+		move /y "!Drive::=!:\*.vbs" "%~dp0Data\Quarantine\USB\!Drive::=!" > nul 2>&1
+		move /y "!Drive::=!:\.trashes" "!Drive::=!:\" > nul 2>&1
+		if exist "!Drive::=!:\*.lnk" (
+			echo Cleaning failed^^!
+		) else (
+			echo !threatname! was successfully removed from your system.
+			echo.
+			echo It is recommended to run a Batch Antivirus scan to ensure no malware is left on the system.
+			echo.Do you want to run a scan on !Drive::=!: ? (y/n)
+			choice /c:YN /n
+			if !errorlevel!==1 (
+				echo Running scan...
+				BAV "%!Drive::=!:\"
+				exit /b %errorlevel%
+			)
+		)
 	)
+) else (
+	echo.
+	echo Drive is not infected.
+	pause>nul
+	exit /b
 )
+
 
 
 pause>nul
